d18ce58464cda16b7be059fde6e41159915488e5
Darien <dhmtaxandmore@gmail.com>
2026-06-15 10:52:02 -0400

    update: map root path to Coral Gables invitation

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,8 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<CoralGablesOpening />} />
+        <Route path="/hq" element={<App />} />
         <Route path="/coral-gables-opening" element={<CoralGablesOpening />} />
         <Route path="/grand-opening" element={<CoralGablesOpening />} />
         <Route path="/mortgage-opening" element={<CoralGablesOpening />} />
